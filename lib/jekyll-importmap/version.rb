cada9a25e6e4011a3a1e4b5d85b477b1f5aefde9
Nathan Kidd <nathanblenheimkidd@gmail.com>
2024-03-17 21:47:14 +0000

    v0.1.15

--- lib/jekyll-importmap/version.rb
+++ lib/jekyll-importmap/version.rb
@@ -4,7 +4,7 @@
 module Liquid; class Tag; end; end
 
 module Jekyll
-  class ImportmapT4g < Liquid::Tag
-    VERSION = "0.1.14"
+  class ImportmapTag < Liquid::Tag
+    VERSION = "0.1.15"
   end
 end
