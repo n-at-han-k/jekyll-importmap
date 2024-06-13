811b2e354754d4c1fe0da05ea89f0f6ee2fc9bac
Zachary Danger Campbell <zacharydanger@fastmail.com>
2024-06-13 08:34:42 -0500

    don't specify full URL for local assets

--- lib/jekyll-importmap/resolver.rb
+++ lib/jekyll-importmap/resolver.rb
@@ -3,35 +3,8 @@ module Jekyll::Importmap
 
     class Resolver
         def self.path_to_asset(path)
-            return path if path.start_with?('http') 
-            self.url + JS_PATH + path
-        end
-
-        def self.host_or_url
-            if Jekyll.configuration['importmap'] && Jekyll.configuration['importmap']['devurl']
-                Jekyll.configuration['importmap']['devurl']
-            else Jekyll.configuration['url']
-                'https://' + Jekyll.configuration['url']
-            end
-        end
-        def self.base_url
-            if Jekyll.configuration['baseurl']
-                Jekyll.configuration['baseurl']
-            else
-                ''
-            end
-        end
-        #def self.port
-            #if Jekyll.configuration['port'] && Jekyll.configuration['port'].length > 0
-                #':' + Jekyll.configuration['port']
-            #else
-                #''
-            #end
-        #end
-
-        def self.url
-            #self.host_or_url + self.port + self.base_url
-            self.host_or_url + self.base_url
+            return path if path.start_with?('http')
+            Jekyll.configuration['baseurl'] + JS_PATH + path
         end
     end
 end
